3d8d0a9a9b41e23a104e07675e4fef9c23131e09
Reveles Sepúlveda Jesús Ángel <98759850+JARS2003@users.noreply.github.com>
2024-05-06 18:32:29 -0600

    Nuevas modificaciones, base de datos y conecciones de links

--- app/calculadoraFertilizante/calculadora/views.py
+++ app/calculadoraFertilizante/calculadora/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import  TemplateView
 # Create your views here.
 def calculadora(request):
     return render(request, 'calculadora.html')
+
+class BienvenidaView(TemplateView):
+    template_name = 'bienvenida.html'
--- app/calculadoraFertilizante/calculadoraFertilizante/urls.py
+++ app/calculadoraFertilizante/calculadoraFertilizante/urls.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from calculadora.views import BienvenidaView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('calculadora/', include ('calculadora.urls')),
     path('usuario/', include ('usuario.urls')),
-    path('cultivos/', include('cultivos.urls'))
+    path('cultivos/', include('cultivos.urls')),
+    path('', BienvenidaView.as_view(), name='bienvenida'),
 ]
