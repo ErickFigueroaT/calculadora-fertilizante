a8bed1815c35e718d779410b6eb9f817fb58d2ee
Reveles Sepúlveda Jesús Ángel <98759850+JARS2003@users.noreply.github.com>
2024-05-17 22:39:55 -0600

    Se agregaron los links a base, para poder agregar, asi como tambien los campos que faltaban para hacer los calculos

--- app/calculadoraFertilizante/calculadora/views.py
+++ app/calculadoraFertilizante/calculadora/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import  TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from cultivos.models import Cultivo
 # Create your views here.
 def calculadora(request):
-    return render(request, 'calculadora.html')
+    cultivos = Cultivo.objects.all()
+    resultado = 10
+    context = {
+        'object_list': cultivos,
+        'resultado': resultado
+    }
+    return render(request, 'calculadora.html',context)
 
 class BienvenidaView(LoginRequiredMixin,TemplateView):
     template_name = 'bienvenida.html'
